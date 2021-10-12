3701dd2beb97ed8422b13dbe8e7a8906d729fd6a
Eric Lunderberg <elunderberg@octoml.ai>
2021-10-12 10:22:06 -0500

    [TIR] Expose tir.transform.ApplyPhysicalLayout for testing

--- src/tir/transforms/storage_flatten.cc
+++ src/tir/transforms/storage_flatten.cc
@@ -1616,6 +1616,8 @@ PrimFunc StorageFlatten(PrimFunc func, int cache_line_size, bool create_bound_at
 
 namespace transform {
 
+TVM_REGISTER_GLOBAL("tir.transform.ApplyPhysicalLayout").set_body_typed(PhysicalLayoutApply::Pass);
+
 // TODO(tvm-team): consolidate configs to the PassContext
 Pass StorageFlatten(int cache_line_size, bool create_bound_attributes) {
   auto pass_func = [=](PrimFunc f, IRModule m, PassContext ctx) {
